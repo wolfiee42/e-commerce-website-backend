d1e6c154050a25c3fb229520dfc53cc1ad78e9ae
Saif <saifalislam2022@gmail.com>
2025-07-04 05:12:03 +0600

    fix: prod fronend url added in cors origin

--- app.ts
+++ app.ts
@@ -14,7 +14,10 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://gilded-alfajores-5d5d0c.netlify.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   }),
